22b23ee0d0c20182e5dd91f013fac15a710e19d9
Danilo de Lucio <47226196+danilodelucio@users.noreply.github.com>
2022-01-04 08:00:57 -0300

    Módulo D - Condições Aula 12

---
+++ aula11/ex008.js
@@ -0,0 +1,7 @@
+var vel = 59.8
+console.log(`A velocidade do seu carro é ${vel}KM.`)
+
+if (vel > 60){
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
